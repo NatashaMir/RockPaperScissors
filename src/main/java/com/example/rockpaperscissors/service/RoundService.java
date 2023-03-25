dd599db12f85d1af6daa48038bfe3faf260dc870
Nataliia Miroshnychenko <nataliia.mirosh@gmail.com>
2023-03-26 00:49:20 +0300

    Add service interface and class

---
+++ src/main/java/com/example/rockpaperscissors/service/RoundService.java
@@ -0,0 +1,8 @@
+package com.example.rockpaperscissors.service;
+
+import com.example.rockpaperscissors.model.Round;
+
+public interface RoundService {
+
+    Round playRound();
+}
---
+++ src/main/java/com/example/rockpaperscissors/service/RoundServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.rockpaperscissors.service;
+
+import com.example.rockpaperscissors.model.Round;
+
+public class RoundServiceImpl implements RoundService{
+    @Override
+    public Round playRound() {
+        return null;
+    }
+}
