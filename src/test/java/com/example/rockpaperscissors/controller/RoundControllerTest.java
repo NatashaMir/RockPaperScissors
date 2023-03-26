6162f91e142076aceccb74b5d51982aed33b08be
Nataliia Miroshnychenko <nataliia.mirosh@gmail.com>
2023-03-26 11:18:03 +0300

    Refactor RoundControllerTest class

--- src/test/java/com/example/rockpaperscissors/controller/RoundControllerTest.java
+++ src/test/java/com/example/rockpaperscissors/controller/RoundControllerTest.java
@@ -8,6 +8,7 @@ import com.example.rockpaperscissors.service.RoundService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -22,24 +23,25 @@ class RoundControllerTest {
     @Mock
     private RoundService roundService;
 
+    @InjectMocks
+    private RoundController roundController;
+
     @Test
     void testNewGame_SameGame() {
-        final Game game = new RoundController(roundService).newGame();
+        Game game = roundController.newGame();
         Assertions.assertEquals(new Game(1L, Collections.emptyList()), game);
     }
 
     @Test
     void testNewGame_DifferentGames() {
-        final RoundController roundController = new RoundController(roundService);
-        final Game gameFirst = roundController.newGame();
-        final Game gameSecond = roundController.newGame();
+        Game gameFirst = roundController.newGame();
+        Game gameSecond = roundController.newGame();
         Assertions.assertEquals(new Game(1L, Collections.emptyList()), gameFirst);
         Assertions.assertEquals(new Game(2L, Collections.emptyList()), gameSecond);
     }
 
     @Test
     void testPlayRound_FindGame() {
-        final RoundController roundController = new RoundController(roundService);
         Game game = roundController.newGame();
         Game actual = roundController.playRound(game.getGameId());
         Assertions.assertEquals(game, actual);
@@ -48,7 +50,6 @@ class RoundControllerTest {
     @Test
     void testPlayRound_GetGame(){
         when(roundService.playRound()).thenReturn(new Round(Choice.ROCK, Choice.PAPER, Result.SECOND_PLAYER_WIN));
-        RoundController roundController = new RoundController(roundService);
         Game game = roundController.newGame();
         Game actual = roundController.playRound(game.getGameId());
         Assertions.assertEquals(List.of(new Round(Choice.ROCK, Choice.PAPER, Result.SECOND_PLAYER_WIN)), actual.getRounds());
