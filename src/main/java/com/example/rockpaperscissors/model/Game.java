63d6e167d989809f3966f7a29bd16f7c55a6c04c
Nataliia Miroshnychenko <nataliia.mirosh@gmail.com>
2023-03-27 23:16:17 +0300

    Refactor Game class

--- src/main/java/com/example/rockpaperscissors/model/Game.java
+++ src/main/java/com/example/rockpaperscissors/model/Game.java
@@ -2,17 +2,15 @@ package com.example.rockpaperscissors.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Game {
 
-    private Long gameId;
-    private List<Round> rounds;
+    private final Long gameId;
+    private final List<Round> rounds;
 
     public synchronized List<Round> getRounds(){
         return List.copyOf(rounds);
