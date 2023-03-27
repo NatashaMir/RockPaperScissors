4752c43b34b6bad1c17da87b9f1b208246b58c83
Nataliia Miroshnychenko <nataliia.mirosh@gmail.com>
2023-03-27 23:15:40 +0300

    Add tests to RoundControllerIntegrationTest

--- src/test/java/com/example/rockpaperscissors/controller/RoundControllerIntegrationTest.java
+++ src/test/java/com/example/rockpaperscissors/controller/RoundControllerIntegrationTest.java
@@ -1,24 +1,15 @@
 package com.example.rockpaperscissors.controller;
 
-import com.example.rockpaperscissors.model.Choice;
-import com.example.rockpaperscissors.model.Game;
-import com.example.rockpaperscissors.service.ChoiceService;
-import com.example.rockpaperscissors.service.RoundService;
-import com.example.rockpaperscissors.service.StatisticService;
-import com.example.rockpaperscissors.service.impl.ChoiceServiceRandom;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,25 +18,47 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(RoundController.class)
 @AutoConfigureMockMvc
+@ComponentScan(basePackages = {"com.example.rockpaperscissors"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class RoundControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private RoundService roundService;
-
-    @MockBean
-    private StatisticService statisticService;
+    @Autowired
+    RoundController roundController;
 
     private final static long GAME_ID = 1L;
+    private final static int ROUND_AMOUNT = 10;
 
     @Test
     void testGetGameNumber() throws Exception {
         mockMvc.perform(get("/restart")).andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$.gameId").value(1))
                 .andExpect(jsonPath("$.rounds").isArray())
-                .andExpect(jsonPath("$.rounds").isEmpty());
+                .andExpect(jsonPath("$.rounds").isEmpty())
+                .andReturn();
+    }
+
+    @Test
+    void testPlayRound_CheckResultGame() throws Exception {
+        roundController.newGame();
+        mockMvc.perform(get("/play/" + GAME_ID)).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.gameId").value(1))
+                .andExpect(jsonPath("$.rounds").isArray())
+                .andExpect(jsonPath("$.rounds[0].firstPlayer").value("ROCK"))
+                .andReturn();
+    }
+
+    @Test
+    void testGetStatistic_CheckRoundsAmount() throws Exception {
+        roundController.newGame();
+        for(int i=0; i<ROUND_AMOUNT; i++) {
+            roundController.playRound(GAME_ID);
+        }
+        mockMvc.perform(get("/statistic")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalRound").value(ROUND_AMOUNT))
+                .andReturn();
     }
 
 }
