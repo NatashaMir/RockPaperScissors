f3fe49d9fd4242decdd73b3672d80b7a8a001c5c
Nataliia Miroshnychenko <nataliia.mirosh@gmail.com>
2023-03-27 23:51:53 +0300

    Add Swagger project annotations

--- src/main/java/com/example/rockpaperscissors/RockPaperScissorsApplication.java
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorsApplication.java
@@ -1,8 +1,19 @@
 package com.example.rockpaperscissors;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Rock-Paper-Scissors Game",
+				version = "1",
+				description = "Test project",
+				contact = @Contact(name = "Nataliia", email = "nataliia.mirosh@hmail.com")
+		)
+)
 @SpringBootApplication
 public class RockPaperScissorsApplication {
 
--- src/main/java/com/example/rockpaperscissors/controller/RoundController.java
+++ src/main/java/com/example/rockpaperscissors/controller/RoundController.java
@@ -5,6 +5,8 @@ import com.example.rockpaperscissors.model.Round;
 import com.example.rockpaperscissors.model.Statistic;
 import com.example.rockpaperscissors.service.RoundService;
 import com.example.rockpaperscissors.service.StatisticService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,8 @@ public class RoundController {
     }
 
     @GetMapping(value = "/restart")
+    @Operation(summary = "Restart game",
+            description = "In this case, a new game is being created. The number of rounds in the new game is zero.")
     public Game newGame() {
         final Game game = new Game(nextGameId.getAndIncrement(), new ArrayList<>());
         gamesMap.put(game.getGameId(), game);
@@ -36,7 +40,10 @@ public class RoundController {
     }
 
     @GetMapping(value = "/play/{gameId}")
-    public Game playRound(@PathVariable Long gameId) {
+    @Operation(summary = "Create new round",
+            description = "As a result of creating a new round, we get an Game object that contains the game ID and " +
+                    "the list of previous rounds and the current round.")
+    public Game playRound(@Parameter(description = "game id") @PathVariable Long gameId) {
         final Game game = gamesMap.get(gameId);
         final Round newRound = roundService.playRound();
         game.addRound(newRound);
@@ -45,6 +52,10 @@ public class RoundController {
     }
 
     @GetMapping(value = "/statistic")
+    @Operation(summary = "Generating game statistics",
+            description = "Generating game statistics based on the results of all games. The statistics object contains " +
+                    "the following fields: the total number of rounds, the number of wins of the first player, the " +
+                    "number of wins of the second player, the number of games drawn.")
     public Statistic getStatistic() {
         return statisticService.getStatistic();
     }
